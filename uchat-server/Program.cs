a25e7e0756e4b51da7a4c15e4ec51e377a910a3a
IlyaOlkh <ilya20050@gmail.com>
2025-12-07 20:51:54 +0200

    fix

--- uchat-server/Program.cs
+++ uchat-server/Program.cs
@@ -64,9 +64,10 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        policy.AllowAnyOrigin()
+        policy.SetIsOriginAllowed(_ => true)
               .AllowAnyHeader()
-              .AllowAnyMethod();
+              .AllowAnyMethod()
+              .AllowCredentials();
     });
 });
 
